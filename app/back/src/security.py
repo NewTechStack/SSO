dbdf2dbbc48628c063057a51db00f9aead305863
SCcagg5 <sccagg5@gmail.com>
2022-12-21 17:25:33 +0100

    Update security.py

--- app/back/src/security.py
+++ app/back/src/security.py
@@ -1,51 +1,68 @@
+import datetime
+import jwt
+
+# Import necessary modules from the cryptography library
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
+# Import necessary modules from the barrel library
 from barrel import Commons, Error, DB, DictObject, StrObject
 
-import datetime
-import jwt
-
 class RSAKey(DB):
+    """
+    Class to generate and store RSA keys.
+    """
+
     def __init__(self):
+        # Define the model for storing the keys
         self.model = DictObject("system", {
             "private": StrObject("system"),
             "public": StrObject("system"),
         })
-        super().__init__(id = "000")
+        super().__init__(id="000")
+        # If the keys do not already exist in the database, generate and store new keys
         if not self._exist():
             self.new()
             self.create()
         self.checkout()
 
     def new(self):
+        """
+        Generate and store new RSA keys.
+        """
+        # Generate a new RSA key
         key = rsa.generate_private_key(
             backend=crypto_default_backend(),
             public_exponent=65537,
             key_size=2048
         )
-
+        # Convert the private key to a PEM-formatted string
         private_key = key.private_bytes(
             crypto_serialization.Encoding.PEM,
             crypto_serialization.PrivateFormat.PKCS8,
             crypto_serialization.NoEncryption()
         ).decode("utf-8")
-
+        # Convert the public key to an OpenSSH-formatted string
         public_key = key.public_key().public_bytes(
             crypto_serialization.Encoding.OpenSSH,
             crypto_serialization.PublicFormat.OpenSSH
         ).decode("utf-8")
-
+        # Store the keys in the database
         self.model.change_data(['private'], private_key)
         self.model.change_data(['public'], public_key)
         self.data = self.model.formating()
 
     def private(self):
+        """
+        Return the private key.
+        """
         return self.data['data']['private']['data']
 
     def public(self):
-        print(f"using private key {self.data['id']}")
+        """
+        Return the public key.
+        """
         return self.data['data']['public']['data']
 
 class Token():
